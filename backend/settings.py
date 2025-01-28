b463e98db3bb2a553f3f70fb92fb0c72f7820aa6
Daniel Felipe Montoya Ramirez <dmontoya.web@gmail.com>
2025-01-28 11:23:25 -0500

    Se ajustan variables de entorno de Base de datos

--- backend/settings.py
+++ backend/settings.py
@@ -70,13 +70,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("DATABASE_HOST"),
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "PORT": "5432",
     }
 }
 
